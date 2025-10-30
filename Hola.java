e7146127dc2742d6d567022088114959e9fceffe
Pedro Juan <pgareli942@g.educaand.es>
2025-10-30 12:37:37 +0100

    Proyecto VSC-UBUNTO version 1.1

--- Hola.java
+++ Hola.java
@@ -4,7 +4,14 @@ public class Hola {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         
+        System.out.print("Introduzca su nombre: ");
         String nombre = sc.nextLine();
-        System.out.print("¡Hola " + nombre + "!");
+        System.out.print("Introduzca su primer apellido: ");
+        String apellido1 = sc.nextLine();
+        System.out.print("Introduzca su segundo apellido: ");
+        String apellido2 = sc.nextLine();
+        System.out.print("Introduzca su numero favorito: ");
+        int numero = sc.nextInt();
+        System.out.print("¡Hola " + nombre + " " + apellido1 + " " + apellido2 + "!" + "¡El " + numero + " es un número muy interesante!");
     }
 }
